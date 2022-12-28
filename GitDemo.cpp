8e520c5edf94c0e3bdf43d5769806eae3524d17f
MSang2k <sangnm@2105.com>
2022-12-28 23:16:58 +0700

    The third commit

--- GitDemo.cpp
+++ GitDemo.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include "MyMath.h"
+#include "MyPrint.cpp"
 
 using namespace std;
 
 int main()
 {
     cout << "Nguyen Minh Sang" << endl;
+    int Sum = sum(6, 9);
+    print(Sum);
     return 0;
 }
 
